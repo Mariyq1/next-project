76d793db0bb6d11a80830d5ab17de01d8dab6115
Mariyq1 <m4697597@gmail.com>
2023-03-08 02:56:40 +0100

    Styling and add scss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Heading from "@/components/Heading";
-
+import styles from "../styles/Home.module.scss";
 
 const Home = () =>(
+  <div className={styles.wrapper}>
   <Heading text="Hello world"/>
+  </div>
 );
 
 export default Home;
