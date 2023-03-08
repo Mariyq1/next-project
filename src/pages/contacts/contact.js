8d0a3bedfe3cafeb43b98e156e9067804cad311e
Mariyq1 <m4697597@gmail.com>
2023-03-08 02:34:41 +0100

    Create next app with components

---
+++ src/pages/contacts/contact.js
@@ -0,0 +1,10 @@
+import Heading from "@/components/Heading";
+
+const Contact = () =>(
+<>
+<Heading text="Jack"/>
+  
+</>
+);
+
+export default Contact; 
---
+++ src/pages/posts.js
@@ -0,0 +1,12 @@
+import Heading from "@/components/Heading";
+
+
+const Posts = () =>(
+<>
+<Heading text="Posts list"/>
+ 
+  <p>This post you can see any time</p>
+</>
+);
+
+export default Posts; 
