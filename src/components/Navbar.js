7f75f3e1feb7499a8130e4a304a887c5d343f4a1
Mariyq1 <m4697597@gmail.com>
2023-03-08 18:52:38 +0100

    Add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,26 @@
 import styles from "../styles/Navbar.module.scss";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
 
+const navigation = [
+    {id:1, title: 'Home', path:'/'},
+    {id:2, title: 'Posts', path: '/posts'},
+    {id:3, title: 'Contacts', path: '/contacts'}
+]
 const Navbar = () =>{
+    const {pathname} = useRouter();
     return(
         <div className={styles.nav}>
-            <h1>Logo</h1>
+            <div className={styles.logo}>
+                <Image src="/logo.jpg" width="60" height="60" alt="logo"/>
+            </div>
             <div className={styles.links}>
-                <Link href="/"><a >Home</a></Link>
-                <Link href="/posts"><a >Posts</a></Link>
-                <Link href="/contacts"><a >Contacts</a></Link>
+                {navigation.map(({id, title, path})=>(
+                    <Link legacyBehavior key={id} href={path} >
+                        <a className={pathname === path ? styles.active : null}>{title}</a>
+                    </Link>
+                ))}
             </div>
         </div>
     )
